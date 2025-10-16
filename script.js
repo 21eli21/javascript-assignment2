09554f1306d9634385f1e9c9f520ff9082d3dd8d
21eli21 <elmiras67156@gmail.com>
2025-10-16 12:35:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log(5452)
+let mybtn =document.querySelector(".btn")
+let myform=document.getElementById("my-form")
+let mybody=document.querySelector("body")
+let myul=document.querySelector(".item")
+
+mybtn.addEventListener("click",function(e){
+e.preventDefault()
+myform.style.backgroundColor="red"
+mybody.classList.add("bg-dark")
+myul.lastElementChild.innerHTML="<h1>hello<h1>"
+})
+
